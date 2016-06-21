a1b2b608ac9e2324428f7798ac25ed810135db04
Per Guth <mail@perguth.de>
2016-06-22 00:14:27 +0200

    testing choo

--- index.js
+++ index.js
@@ -1,66 +1,35 @@
-var hyperlog = require('hyperlog')
-var levelup = require('levelup')
-var steed = require('steed')
+const choo = require('choo')
+const app = choo()
 
-var db1 = levelup('./db1', { db: require('memdown') })
-var db2 = levelup('./db2', { db: require('memdown') })
-var db3 = levelup('./db3', { db: require('memdown') })
-
-var log1 = hyperlog(db1)
-var log2 = hyperlog(db2)
-var log3 = hyperlog(db3)
-
-steed.waterfall([
-  function (cb) { // a
-    log1.add(null, 'a: hello', function (err, node) {
-      console.log('inserted node', node)
-      cb(null, node)
-    })
-  },
-  function (node, cb) { // a
-    log1.add([node.key], 'a: world', function (err, node) {
-      console.log('inserted new node', node)
-      cb()
-    })
-  },
-  function (cb) { // b
-    log2.add(null, 'b: world', function (err, node) {
-      console.log('inserted new node', node)
-      cb()
-    })
-  },
-  function (cb) {
-    console.log('---- syncing a:b -----')
-
-    var replicate1 = log1.replicate()
-    var replicate2 = log2.replicate()
-
-    replicate1.pipe(replicate2).pipe(replicate1)
-
-    replicate1.on('end', function () {
-      console.log('replication ended')
-      cb()
-    })
-  },
-  function (cb) {
-    console.log('---- syncing a:c -----')
-
-    var replicate1 = log1.replicate()
-    var replicate3 = log3.replicate()
-
-    replicate1.pipe(replicate3).pipe(replicate1)
-
-    replicate1.on('end', function () {
-      console.log('replication ended')
-      cb()
-    })
-  },
-  function (cb) { // c
-    log1.add(null, 'a: hello', function (err, node) {
-      console.log('inserted node', node)
-      cb(null, node)
-    })
+var state = {}
+var reducers = {
+  print: (action, state) => ({ title: action.payload })
+}
+var subscriptions = [
+  (send) => setInterval(() => {send('print', { payload: 'dawg?' })}, 1000)
+]
+var effects = {
+  print: (action, reducer) => {
+    console.log(action.payload)
+    console.log(reducer)
   }
-], function (err, res) {
-  log1.createReadStream().on('data', buff => console.log(buff.value.toString()))
-})
+}
+
+var model = {state, reducers, subscriptions, effects}
+app.model(model)
+
+const mainView = (params, state, send) => choo.view`
+  <main>
+    <h1>blob-stream</h1>
+    <input
+      type="text"
+      oninput=${(e) => send('update', { value: e.target.value })}>
+  </main>
+`
+
+app.router((route) => [
+  route('/', mainView)
+])
+
+const tree = app.start()
+document.body.appendChild(tree)
