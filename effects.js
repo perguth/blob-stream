5fa407c60ff3ba2220320ba9013d2e737f6bf3af
Per Guth <mail@perguth.de>
2016-07-12 19:13:37 +0200

    updated choo, tried to call done() in effects

--- effects.js
+++ effects.js
@@ -3,20 +3,22 @@
 module.exports = {
   // asynchronous functions that emit an action when done
   // Signature of (data, state, send, done)
-  fileSelected: (data, state, send) => {
+  fileSelected: (data, state, send, done) => {
     let fileList = data.fileList
     for (let prop in fileList) {
       if (isNaN(+prop)) continue
-      readFile(fileList.item(prop), send)
+      readFile(fileList.item(prop), send, done)
     }
   },
   handleFileBuffer: (fileBuffer) => console.log(fileBuffer)
 }
 
-function readFile (handle, send) {
+function readFile (handle, send, done) {
   let reader = new FileReader()
   reader.onload = () => {
     send('handleFileBuffer', {fileBuffer: reader.result})
+    // throws but shoudnt:
+    // done()
   }
   reader.readAsArrayBuffer(handle)
 }
