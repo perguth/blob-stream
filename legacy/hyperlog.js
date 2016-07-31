9de8f893c62290b0a943d4a9a6c64617ae759d26
Per Guth <mail@perguth.de>
2016-07-31 22:08:30 +0200

    switch to headless mode `export HEADLESS=true` to make it work in node

--- legacy/hyperlog.js
+++ legacy/hyperlog.js
@@ -1,14 +1,14 @@
 var hyperlog = require('hyperlog')
 var levelup = require('level')
 var leveldb = levelup('./ephemeral/hyperlog_state')
-var WebTorrent = require('webtorrent')
+var WebTorrentHybrid = require('webtorrent-hybrid')
 var log = hyperlog(leveldb)
 var effects = require('../model/effects')
 var subscriptions = require('../model/subscriptions')({
-  wrtc: require('electron-webrtc')()
+  wrtc: require('electron-webrtc')({headless: true})
 }, log)
 
-var swarm = new WebTorrent()
+var swarm = new WebTorrentHybrid()
 var state = {log, swarm}
 
 module.exports = {
@@ -17,15 +17,25 @@ module.exports = {
     var data = {'file': file}
     var send = (send, data) => {
       console.log('create torrent', data.magnetLink)
-      effects[send](data, state, null, err => !err && console.log(err))
+      effects[send](data, state, null, err => err && console.log(err))
     }
-    effects['create torrent'](data, state, send, err => !err || console.log(err))
+    effects['create torrent'](data, state, send, err => err && console.log(err))
   }
 }
 
 var findWebRTCPeers = subscriptions.find(elem => elem.name === 'findWebRTCPeers')
 
-findWebRTCPeers((send, data) => {
-  console.log('findWebRTCPeers:', send, data)
-  effects[send](data, state, null, err => !err && console.log(err))
+findWebRTCPeers((next, data) => {
+  console.log('findWebRTCPeers ->', next)
+  effects[next](data, state, null, err => err && console.log(err))
+})
+
+var subscribeToLogChanges = subscriptions.find(elem => elem.name === 'subscribeToLogChanges')
+
+subscribeToLogChanges((next, data) => {
+  console.log('subscribeToLogChanges ->', next, data)
+  var send = (next, data) => {
+    console.log('... ->', next)
+  }
+  effects[next](data, state, send, err => err && console.log(err))
 })
