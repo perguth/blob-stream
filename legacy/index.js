0940ab8b8d162249e5828bc219a6b8c24ea84d89
Per Guth <mail@perguth.de>
2016-07-31 13:06:17 +0200

    switched from dirty to flat-file-db

--- legacy/index.js
+++ legacy/index.js
@@ -1,9 +1,7 @@
+
 var fs = require('fs')
 var chokidar = require('chokidar') // file watcher
-var dirty = require('dirty') // DB
-// var steed = require('steed') // async handling
-// var rusha = new (require('rusha'))(24)
-// var hash = input => rusha.digestFromString(input)
+var flatfile = require('flat-file-db')
 
 var path = require('path').dirname(require.main.filename) + '/'
 var folder = path + 'ephemeral/'
@@ -13,21 +11,21 @@ if (!fs.existsSync(folder)) {
   fs.mkdirSync(folder)
 }
 
-var db = dirty(folder + 'known_files.db')
+var db = flatfile(folder + 'known_files.db')
 
-db.on('load', () => {
+db.on('open', () => {
   var watcher = chokidar.watch(folder, {ignored: /\.DS_Store/, depth: 0})
 
   watcher.on('addDir', path => {
     if (db.get(path)) return
-    if (!db.get(path)) db.set(path, true)
+    db.put(path, true)
     console.log('subdirectories are not supported!', db.get(path))
   })
 
   var newFile = path => {
     if (db.get(path)) return
-    if (!db.get(path)) db.set(path, true)
-    console.log('file added: ', path, db.get(path))
+    console.log('adding file: ', path)
+    db.put(path, true)
   }
   watcher.on('change', newFile)
   watcher.on('add', newFile)
